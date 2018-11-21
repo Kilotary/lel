a3f4145f745384df227ea4d688a25e73bef698b8
Kilotary <crazymech@mail.ru>
2018-11-21 17:45:11 +0600

    'c'

--- lolkekbot.py
+++ lolkekbot.py
@@ -1,6 +1,8 @@
 import requests
 import lxml.html as html
-import datetime
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+
 
 class LelKekBot:
 
@@ -19,7 +21,8 @@ class LelKekBot:
         getResult = self.getUpdates()
         if len(getResult)>0:
             lastUpdate = getResult[-1]
-        else: lastUpdate = getResult[len(getResult)]    
+        else:
+            lastUpdate = 0    
         return lastUpdate
 
     def sendMess(self, chatId, text):
@@ -27,40 +30,47 @@ class LelKekBot:
         method = 'sendMessage'
         response = requests.post(self.apiUrl+method,params)
 
-    def getYoutubeFirstResult(self, url, searchparam):
-        search = "/results?search_query="
-        req = requests.get(url + search + searchparam )
-        ydoc = html.document_fromstring(req.text())
-        for a in ydoc.xpath("//a"):
-            res = a.get('href')
-            if res[1:6] == 'watch':
-                break
-        return url + res    
+    def getVideo(self, query, where):
+        searchResponse = where.search().list(
+                q = query,
+                part='snippet',
+                maxResults=5
+                ).execute()
+
+        for searchRes in searchResponse.get('items',[]):
+            if searchRes['id']['kind']=='youtube#video':
+                return searchRes['id']['videoId']
+        
+        return 'cocиписос'       
 
 token = "700076471:AAG9HAUfDiPKH4QWaD995F68Pga2cE2q5KA"
 bot = LelKekBot(token)
-youtubeURL = "http://youtube.com"   
+
+DEVELOPER_KEY = 'AIzaSyD3q2zJLDca75xklszgqsMsZIONdR9nUwA'
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
 def main():
     newOffset = None
-
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+    
     while True:
         bot.getUpdates(newOffset)
         lastUpdate = bot.getLastUpdate()
-
-        lastUpdateId = bot.lastUpdate['update_id']
-        lastChatText = bot.lastUpdate['message']['text']
-        lastChatId = bot.lastUpdate['message']['chat']['id']
-        lastChatName = bot.lastUpdate['message']['chat']['first_name']
-        lastChatTextSplit = lastChatText.split()
-        searchparam = ''
-    
-        if lastChatTextSplit[0].lower() == '!vid':
-            for p in lastChatUpdateSplit:
-                searchparam += p + '+'
-            searchparam = searchparam[0,-1]
-            res = bot.getYoutubeFirstResult(youtubeURL, searchparam)
-            bot.sendMess(lastChatId, res)
-            
+        
+        if lastUpdate ==0:continue
+        
+        lastUpdateId = lastUpdate['update_id']
+        lastChatText = lastUpdate['message']['text']
+        lastChatId = lastUpdate['message']['chat']['id']
+        #lastChatName = lastUpdate['message']['chat']['first_name']
+        lastChatTextSplit = lastChatText.split(maxsplit=1)
+ 
+        if lastChatTextSplit[0].lower() == '!vid' and  len(lastChatTextSplit)>1:
+            h = bot.getVideo(lastChatTextSplit[1], youtube)        
+            bot.sendMess(lastChatId, "http://youtube.com/watch?v="+h)
+  
         newOffset = lastUpdateId +1    
 
 if __name__ == '__main__':
